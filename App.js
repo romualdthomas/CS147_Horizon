66ae4edf351b4eda2d23afa50937d3c7f944fb94
Alex Thomas <romualdthomas@alexs-mbp.myfiosgateway.com>
2022-11-23 21:52:27 -0500

    First Commit

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import colors from './assets/colors/colors';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Mont-Bold': require('./assets/fonts/Mont-Trial-Bold.ttf'),
+    'Mont-Black': require('./assets/fonts/Mont-Trial-Black.ttf'),
+    'Mont-Heavy': require('./assets/fonts/Mont-Trial-Heavy.ttf'),
+    'Mont-Light': require('./assets/fonts/Mont-Trial-ExtraLight.ttf'),
+    'Mont-Regular': require('./assets/fonts/Mont-Trial-Regular.ttf')
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.title}>
+        <Text style={{ fontFamily: 'Mont-Heavy', color: colors.black }}> Career Backpack  😁</Text>
+        <Icon name="ios-arrow-forward-outline" size={30} color={colors.black} />
+        <StatusBar style="auto" />
+      </View>
+      <View style={styles.container}>
+        <Text style={{ fontFamily: 'Mont-Regular', color: colors.black }}>
+          Connecting individuals with shared backgrounds as they look to make
+          <Text style={{ fontFamily: 'Mont-Regular', color: colors.evergreen }}>career</Text> pivots
+        </Text>
+      </View>
+    </>
   );
 }
 
@@ -17,4 +37,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    alignSelf: 'flex-end',
+    marginRight: 210,
+    marginTop: 70,
+    //position: 'absolute', // add if dont work with above
+  }
 });
---
+++ assets/colors/colors.js
@@ -0,0 +1,11 @@
+const colors = {
+    white: '#FFFFFF',
+    black: '#000000',
+    dark_orange: '#E3673C',
+    dark_yellow: '#F2BA4C',
+    mid_purple: '#BD7BC4',
+    mid_blue: '#6E89E2',
+    evergreen: '#6EBB86'
+}
+
+export default colors;
